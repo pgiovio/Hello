813e08cc371bc4aed511c6fda0d4a4bdea0b2ad5
pgiovio <p_giovio@hotmail.com>
2017-11-28 05:33:12 -0300

    pepe

---
+++ example/code.c
@@ -0,0 +1,9 @@
+void getComm(unsigned int len, char * src)
+{
+	 unsigned int size;
+	  size = len - 2;
+	   char * comm = (char *) malloc(size + 1);
+	    memcypy(comm, src, size);
+	     return;
+}
+
